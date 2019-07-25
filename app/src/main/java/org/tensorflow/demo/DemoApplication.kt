c333192edded4c3501b2c2b3d9a8cef3359c5f82
SOUP <soupyong@gmail.com>
2019-07-25 20:31:02 +0900

    Plant Timber debug tree

---
+++ app/src/main/java/org/tensorflow/demo/DemoApplication.kt
@@ -0,0 +1,12 @@
+package org.tensorflow.demo
+
+import android.app.Application
+import timber.log.Timber
+
+class DemoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
