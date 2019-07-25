62d6bbcd6a6a2d2bf290136d8558e2c8df8a357a
SOUP <soupyong@gmail.com>
2019-07-25 19:58:26 +0900

    Clean up codes as CodeStyle setting

--- app/src/main/java/org/tensorflow/demo/OverlayView.java
+++ app/src/main/java/org/tensorflow/demo/OverlayView.java
@@ -27,27 +27,29 @@ import java.util.List;
  * A simple View providing a render callback to other classes.
  */
 public class OverlayView extends View {
-  private final List<DrawCallback> callbacks = new LinkedList<DrawCallback>();
 
-  public OverlayView(final Context context, final AttributeSet attrs) {
-    super(context, attrs);
-  }
+    private final List<DrawCallback> callbacks = new LinkedList<DrawCallback>();
 
-  /**
-   * Interface defining the callback for client classes.
-   */
-  public interface DrawCallback {
-    public void drawCallback(final Canvas canvas);
-  }
-
-  public void addCallback(final DrawCallback callback) {
-    callbacks.add(callback);
-  }
-
-  @Override
-  public synchronized void draw(final Canvas canvas) {
-    for (final DrawCallback callback : callbacks) {
-      callback.drawCallback(canvas);
+    public OverlayView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
     }
-  }
+
+    /**
+     * Interface defining the callback for client classes.
+     */
+    public interface DrawCallback {
+        public void drawCallback(final Canvas canvas);
+    }
+
+    public void addCallback(final DrawCallback callback) {
+        callbacks.add(callback);
+    }
+
+    @Override
+    public void draw(final Canvas canvas) {
+        super.draw(canvas);
+        for (final DrawCallback callback : callbacks) {
+            callback.drawCallback(canvas);
+        }
+    }
 }
