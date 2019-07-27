2dbb85f209722a1503fd794b949f677e46aded0d
SOUP <soupyong@gmail.com>
2019-07-28 00:30:41 +0900

    Convert StylizeActivity to kotlin codes

---
+++ app/src/main/java/org/tensorflow/demo/StylizeActivity.kt
@@ -0,0 +1,311 @@
+/*
+ * Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.demo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.os.Build
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.Button
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.otaliastudios.cameraview.CameraView
+import com.otaliastudios.cameraview.frame.Frame
+import timber.log.Timber
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * Sample activity that stylizes the camera preview according to "A Learned Representation For
+ * Artistic Styles" (https://arxiv.org/abs/1610.07629)
+ */
+class StylizeActivity : AppCompatActivity() {
+
+    private lateinit var stylize: Stylize
+
+    private lateinit var cameraView: CameraView
+
+    private lateinit var preview: ImageView
+
+    private lateinit var adapter: ImageGridAdapter
+
+    // Start at a medium size, but let the user step up through smaller sizes so they don't get
+    // immediately stuck processing a large image.
+    private val SIZES = intArrayOf(128, 192, 256, 384, 512, 720)
+    private var initializedSize = 0
+    private var desiredSize = SIZES[5]
+    private var desiredSizeIndex = -1
+
+    private var croppedBitmap: Bitmap? = null
+
+    private var computing = false
+
+    private var frameToCropTransform: Matrix? = null
+
+    private var lastOtherStyle = 1
+
+    private var allZero = false
+
+    private val gridTouchAdapter = object : OnTouchListener {
+        private var slider: ImageSlider? = null
+
+        override fun onTouch(v: View, event: MotionEvent): Boolean {
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN ->
+                    for (i in 0 until Styles.count) {
+                        val child = adapter.getItem(i)
+                        val rect = Rect()
+                        child.getHitRect(rect)
+                        if (rect.contains(event.x.toInt(), event.y.toInt())) {
+                            slider = child
+                            slider?.setHighlight(true)
+                        }
+                    }
+
+                MotionEvent.ACTION_MOVE ->
+                    if (slider != null) {
+                        val rect = Rect()
+                        slider?.getHitRect(rect)
+
+                        val newSliderVal = min(
+                            1.0,
+                            max(0.0, 1.0 - (event.y - slider!!.top) / slider!!.height)
+                        ).toFloat()
+
+                        setStyle(slider!!, newSliderVal)
+                    }
+
+                MotionEvent.ACTION_UP ->
+                    if (slider != null) {
+                        slider?.setHighlight(false)
+                        slider = null
+                    }
+            }
+            return true
+        }
+    }
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stylize)
+
+        stylize = Stylize(assets)
+
+        cameraView = findViewById(R.id.cameraView)
+        preview = findViewById(R.id.preview)
+
+        val sizeButton: Button = findViewById(R.id.sizeButton)
+        sizeButton.text = "$desiredSize"
+        sizeButton.setOnClickListener {
+            desiredSizeIndex = (desiredSizeIndex + 1) % SIZES.size
+            desiredSize = SIZES[desiredSizeIndex]
+            sizeButton.text = "$desiredSize"
+        }
+        }
+
+        adapter = ImageGridAdapter(this, Styles.thumbnails)
+        val grid: GridView = findViewById(R.id.grid_layout)
+        grid.adapter = adapter
+        grid.setOnTouchListener(gridTouchAdapter)
+
+        setStyle(adapter.getItem(0), 1.0f)
+
+        if (hasPermission()) {
+            bindCamera()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                bindCamera()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)) {
+                Toast.makeText(
+                    this,
+                    "Camera AND storage permission are required for this demo",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            requestPermissions(arrayOf(PERMISSION_CAMERA), PERMISSIONS_REQUEST)
+        }
+    }
+
+    private fun bindCamera() {
+        cameraView.setLifecycleOwner(this)
+        cameraView.addFrameProcessor { frame ->
+            if (computing) return@addFrameProcessor
+            computing = true
+
+            val stylizedImage = getStylizedImageFrom(frame)
+
+            computing = false
+
+            runOnUiThread { preview.setImageBitmap(stylizedImage) }
+        }
+    }
+
+    public override fun onDestroy() {
+        cameraView.clearFrameProcessors()
+        super.onDestroy()
+    }
+
+    private fun getStylizedImageFrom(frame: Frame): Bitmap {
+        val previewWidth = frame.size.width
+        val previewHeight = frame.size.height
+
+        if (desiredSize != initializedSize) {
+            Timber.i(
+                "Initializing at size preview size %dx%d, stylize size %d",
+                previewWidth, previewHeight, desiredSize
+            )
+            croppedBitmap = Bitmap.createBitmap(desiredSize, desiredSize, Bitmap.Config.ARGB_8888)
+
+            frameToCropTransform = ImageUtils.getTransformationMatrix(
+                previewWidth, previewHeight,
+                desiredSize, desiredSize,
+                0, true
+            )
+            initializedSize = desiredSize
+        }
+
+        val bitmap = FirebaseVisionImage
+            .fromByteArray(
+                frame.data, FirebaseVisionImageMetadata.Builder()
+                    .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                    .setWidth(frame.size.width)
+                    .setHeight(frame.size.height)
+                    .setRotation(FirebaseVisionImageMetadata.ROTATION_90)
+                    .build()
+            )
+            .bitmap
+
+        val canvas = Canvas(croppedBitmap!!)
+        canvas.drawBitmap(bitmap, frameToCropTransform!!, null)
+
+        return stylize.stylize(croppedBitmap!!, desiredSize)
+    }
+
+    private fun setStyle(slider: ImageSlider, value: Float) {
+        slider.value = value
+
+        val styleCount = Styles.count
+
+        if (NORMALIZE_SLIDERS) {
+            // Slider vals correspond directly to the input tensor vals, and normalization is visually
+            // maintained by remanipulating non-selected sliders.
+            var otherSum = 0f
+
+            for (i in 0 until styleCount) {
+                if (adapter.getItem(i) != slider) {
+                    otherSum += adapter.getItem(i).value
+                }
+            }
+
+            if (otherSum > 0) {
+                var highestOtherVal = 0f
+                val factor = if (otherSum > 0f) (1f - value) / otherSum else 0f
+                for (i in 0 until styleCount) {
+                    val child = adapter.getItem(i)
+                    if (child == slider) {
+                        continue
+                    }
+                    val newVal = child.value * factor
+                    child.value = if (newVal > 0.01f) newVal else 0.0f
+
+                    if (child.value > highestOtherVal) {
+                        lastOtherStyle = i
+                        highestOtherVal = child.value
+                    }
+                }
+            } else {
+                // Everything else is 0, so just pick a suitable slider to push up when the
+                // selected one goes down.
+                if (adapter.getItem(lastOtherStyle) == slider) {
+                    lastOtherStyle = (lastOtherStyle + 1) % styleCount
+                }
+                adapter.getItem(lastOtherStyle).value = 1f - value
+            }
+        }
+
+        val lastAllZero = allZero
+        var sum = 0f
+        for (i in 0 until styleCount) {
+            sum += adapter.getItem(i).value
+        }
+        allZero = sum == 0f
+
+        for (item in adapter.items) {
+            item.setAllZero(allZero)
+        }
+
+        // Now update the values used for the input tensor. If nothing is set, mix in everything
+        // equally. Otherwise everything is normalized to sum to 1.0.
+        val styleValues = FloatArray(styleCount)
+        for (i in 0 until styleCount) {
+            styleValues[i] = if (allZero) 1f / styleCount else adapter.getItem(i).value / sum
+
+            if (lastAllZero != allZero) {
+                adapter.getItem(i).postInvalidate()
+            }
+        }
+        stylize.setStyleValues(styleValues)
+    }
+
+    companion object {
+
+        private const val PERMISSIONS_REQUEST = 1
+
+        private const val PERMISSION_CAMERA = Manifest.permission.CAMERA
+
+        // Whether to actively manipulate non-selected sliders so that sum of activations always appears
+        // to be 1.0. The actual style input tensor will be normalized to sum to 1.0 regardless.
+        private const val NORMALIZE_SLIDERS = true
+    }
+}
